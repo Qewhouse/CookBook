3dfa3bf1ed4692cfaf3f70a530d12c75cba5f0fc
Mark Raitman <markraitman@mail.ru>
2023-03-03 18:37:31 +0300

    Added onboarding screen

--- CookBook/Controllers/SearchViewController.swift
+++ CookBook/Controllers/SearchViewController.swift
@@ -8,27 +8,27 @@
 import UIKit
 
 class SearchViewController: UIViewController  {
-   
 
-    
+
+
     //MARK: - Search + TableView
     weak var delegate : SearchViewController?
     var searchController = UISearchController()
     var tableView = UITableView()
-    
-   
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         definesPresentationContext = true
         view.backgroundColor = .white
         view.translatesAutoresizingMaskIntoConstraints = false
-        
+
         searchController = UISearchController(searchResultsController: nil)
         searchController.searchResultsUpdater = self as? any UISearchResultsUpdating
         searchController.obscuresBackgroundDuringPresentation = false
         definesPresentationContext = true
-        
+
         let searchBar = searchController.searchBar
         searchBar.layer.cornerRadius = 11
         searchBar.layer.masksToBounds = true
@@ -40,9 +40,9 @@ class SearchViewController: UIViewController  {
         searchBar.placeholder = "Search recipes"
         searchBar.searchTextField.attributedPlaceholder = NSAttributedString(string: "Search recipes", attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.9764705882, green: 0.8666666667, blue: 0.462745098, alpha: 1) ])
 
-       
+
         view.addSubview(searchBar)
-        
+
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             searchBar.topAnchor.constraint(equalTo: view.topAnchor, constant: 56),
@@ -50,23 +50,23 @@ class SearchViewController: UIViewController  {
             searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -25),
             searchBar.heightAnchor.constraint(equalToConstant: 57)
         ])
-        
+
         tableView = UITableView()
         tableView.dataSource = self as? any UITableViewDataSource
         tableView.delegate = self as? any UITableViewDelegate
         view.addSubview(tableView)
-        
+
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        
+
         ])
     }
-    
+
 }
 
 
